4866687497534c811af1311f4e14960a8de73527
galbb12 <galbb12>
2023-10-29 23:43:54 +0200

    added files to git ignore

---
+++ CMakeFiles/compiled-exe.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/gal/Documents/duplicate_file_resolver/file_metadata.cpp" "CMakeFiles/compiled-exe.dir/file_metadata.cpp.o" "gcc" "CMakeFiles/compiled-exe.dir/file_metadata.cpp.o.d"
+  "/home/gal/Documents/duplicate_file_resolver/main.cpp" "CMakeFiles/compiled-exe.dir/main.cpp.o" "gcc" "CMakeFiles/compiled-exe.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
