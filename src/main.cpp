089edcfaed4acf2715048b6754c49c82a22dc426
galbb12 <galbb12>
2023-10-30 01:16:37 +0200

    need to change hash_generation method

--- src/main.cpp
+++ src/main.cpp
@@ -8,20 +8,24 @@ using namespace std;
 namespace fs = std::filesystem;
 void remove_duplicate(filesystem::path folder_path);
 
-int main(int argc,char* argv[]){
-	//root_run_path is the path which the program will start running on it means that the program will run on all sub directories of the directory in the path
+int main(int argc, char *argv[])
+{
+	// root_run_path is the path which the program will start running on it means that the program will run on all sub directories of the directory in the path
 	string root_run_path = "";
-	if(argc < 2){
-	  filesystem::path cwd = filesystem::current_path();
-          root_run_path = cwd.string();
+	if (argc < 2)
+	{
+		filesystem::path cwd = filesystem::current_path();
+		root_run_path = cwd.string();
 	}
-	else{
-	   root_run_path = argv[1];
+	else
+	{
+		root_run_path = argv[1];
 	}
 
-        filesystem::path folder_path = fs::path(root_run_path);
+	filesystem::path folder_path = fs::path(root_run_path);
 
-        if(!filesystem::exists(folder_path)){
+	if (!filesystem::exists(folder_path))
+	{
 		cout << "The path: " << root_run_path << "doesn't exist" << endl;
 		return -1;
 	}
@@ -32,10 +36,23 @@ int main(int argc,char* argv[]){
 	remove_duplicate(folder_path);
 }
 
-void remove_duplicate(filesystem::path folder_path){
+void remove_duplicate(filesystem::path folder_path)
+{
 	vector<file_metadata> vector_of_files;
-	for(const fs::path& file : fs::recursive_directory_iterator(folder_path)){
-	   vector_of_files.push_back(file_metadata(file));
-	   cout << file << endl;
+	for (const fs::path &file : fs::recursive_directory_iterator(folder_path))
+	{
+		if (!filesystem::is_directory(file))
+		{
+			file_metadata curr_file = file_metadata(file);
+			cout << curr_file.hash_value <<"," << curr_file.path<< endl;
+			for (vector<file_metadata>::iterator ptr = vector_of_files.begin(); ptr < vector_of_files.end(); ptr++)
+			{
+				if ((*ptr).hash_value == curr_file.hash_value)
+				{
+					cout << file << endl;
+				}
+			}
+			vector_of_files.push_back(file_metadata(file));
+		}
 	}
 }
