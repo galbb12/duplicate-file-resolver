1c449c4740ecd7f4927443973c2adc96c09f1589
galbb12 <galbb12>
2023-10-30 01:17:20 +0200

    added todo

--- src/file_metadata.cpp
+++ src/file_metadata.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+//@todo change hash generation method
 file_metadata::file_metadata(filesystem::path _path){
 	file_metadata::path = _path; 
 	file_metadata::file_size = filesystem::file_size(file_metadata::path);
